4ff6b78b2f40af835fe5698d6f033968342841f5
Matviy Bichevuy <matviy.bichevyi@kgemt.org.ua>
2026-05-07 14:39:39 +0300

    Lab 6 : Add ConfigModule

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = parseInt(process.env.PORT ?? "3000", 10);
+
+  await app.listen(port);
+
+  console.log(`Server started on port ${port}`);
 }
+
 bootstrap();
